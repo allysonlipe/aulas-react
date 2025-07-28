5b6ef8742374513ec5b36581513b34aef42b1589
allysonlipe <allysongunner1@hotmail.com>
2025-07-27 23:33:29 -0300

    adicionando a funcao resetar valor

--- src/components/ContadorV2.jsx
+++ src/components/ContadorV2.jsx
@@ -9,12 +9,16 @@ function ContadorV2() {
   const decrementar = () => {
     setContadorV2(contadorV2 - 1);
   };
+  const resetarValor = () => {
+    setContadorV2(0);
+  };
 
   return (
     <div>
       <p>Você clicou {contadorV2} vezes!</p>
       <button onClick={incrementar}> Adicionar 1</button>
       <button onClick={decrementar}> Remover 1</button>
+      <button onClick={resetarValor}> Resetar</button>
     </div>
   );
 }
